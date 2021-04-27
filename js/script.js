e8d73ed7e6d0bd2da9cc3bfdef7ec960726a8504
Ayush <ayush.kumar20@yahoo.in>
2021-04-27 14:16:39 +0530

    Update

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+window.addEventListener("load", function () {
+  setTimeout(function () {
+    document.getElementById('loaderDiv').style.display = 'none';
+  }, 1000);
+})
 function navigateTo(destination){
   window.location = destination;
 }
